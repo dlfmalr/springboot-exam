b771f58d373438c7158268998347fed84ce23e7f
dlfmalr <sonhyeseung05@gmail.com>
2024-04-23 16:16:16 +0900

    1

---
+++ src/main/java/com/example/ms1/note/note/NoteService.java
@@ -0,0 +1,20 @@
+package com.example.ms1.note.note;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class NoteService {
+    private final NoteRepository noteRepository;
+    public Note saveDefault() {
+        Note note = new Note();
+        note.setTitle("new title..");
+        note.setContent("");
+        note.setCreateDate(LocalDateTime.now());
+
+        return noteRepository.save(note);
+    }
+}
